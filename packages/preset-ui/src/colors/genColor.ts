fd87899306d666e4a3d2ce54b5e43ef23694c636
Johnkat Mj <johnkatembue4@gmail.com>
2025-03-02 19:34:46 +0200

    feat: enhance color generation and theme configuration

--- packages/preset-ui/src/colors/genColor.ts
+++ packages/preset-ui/src/colors/genColor.ts
@@ -8,23 +8,7 @@ export const getBrice = (colorMode: uiColorFormat, value: string, colorPrefix?: 
 }
 export const genColorValue = (initialKey: string, colorMode: uiColorFormat, colorPrefix?: string) => {
     return {
-        50: getBrice(colorMode, `${initialKey}-50`, colorPrefix),
-        100: getBrice(colorMode, `${initialKey}-100`, colorPrefix),
-        200: getBrice(colorMode, `${initialKey}-200`, colorPrefix),
-        300: getBrice(colorMode, `${initialKey}-300`, colorPrefix),
-        400: getBrice(colorMode, `${initialKey}-400`, colorPrefix),
-        500: getBrice(colorMode, `${initialKey}-500`, colorPrefix),
-        600: getBrice(colorMode, `${initialKey}-600`, colorPrefix),
-        700: getBrice(colorMode, `${initialKey}-700`, colorPrefix),
-        800: getBrice(colorMode, `${initialKey}-800`, colorPrefix),
-        900: getBrice(colorMode, `${initialKey}-900`, colorPrefix),
-        950: getBrice(colorMode, `${initialKey}-950`, colorPrefix)
-    }
-}
-
-
-export const genGlobalColorValue = (initialKey: string, colorMode: uiColorFormat, colorPrefix?: string) => {
-    return {
+        DEFAULT: getBrice(colorMode, `${initialKey}-DEFAULT`, colorPrefix),
         50: getBrice(colorMode, `${initialKey}-50`, colorPrefix),
         100: getBrice(colorMode, `${initialKey}-100`, colorPrefix),
         200: getBrice(colorMode, `${initialKey}-200`, colorPrefix),
--- packages/preset-ui/src/shortcuts/switch/index.ts
+++ packages/preset-ui/src/shortcuts/switch/index.ts
@@ -13,19 +13,44 @@ const getSwitch = (size: "xs" | "sm" | "md" | "lg") => {
 	[--knob-checked-left:var(--ui-switch-${size}-knob-checked-left,${switchSize.knob.checkedLeft})]
 	`
 }
+
+const getSwitchOutline = (size: "xs" | "sm" | "md" | "lg") => {
+	const switchSize = switchSizeVariants[size]
+	return `[--toggle-width:var(--ui-switch-${size}-w,${switchSize.width})]
+	[--toggle-height:var(--ui-switch-${size}-h,${switchSize.height})]
+	[--knob-size:var(--ui-switch-${size}-knob-size,${switchSize.knob.size})]
+	[--knob-left:calc(var(--ui-switch-${size}-knob-left,${switchSize.knob.left})_-_var(--switch-border-width))]
+	[--knob-top:calc(var(--ui-switch-${size}-knob-top,${switchSize.knob.top})_-_var(--switch-border-width))]
+	[--knob-checked-left:calc(var(--ui-switch-${size}-knob-checked-left,${switchSize.knob.checkedLeft})_-_var(--switch-border-width))]
+	`
+}
 const getSwitchShortcuts = () => {
 	const switchs = {
-		switch: `
-		appearance-none cursor-pointer w-[--toggle-width] h-[--toggle-height] rounded-[var(--toggle-radius,9999px)]
+		switch: `appearance-none cursor-pointer w-[--toggle-width] h-[--toggle-height] rounded-[var(--toggle-radius,9999px)]
 		relative before:absolute before:w-[var(--knob-width,var(--knob-size))] before:h-[var(--knob-height,var(--knob-size))] before:content-empty 
 		before:bg-[--knob-bg] before:rounded-[var(--knob-radius,9999px)] ease-linear transition-all duration-300
 		before:left-[--knob-left] before:top-[--knob-top] checked:before:left-[--knob-checked-left] before:ease-linear 
 		before:transition-all before:duration-300 disabled:opacity-70 disabled:cursor-not-allowed 
 		checked:bg-current checked:before:bg-[var(--knob-bg-checked,var(--knob-bg))]`,
+
+		'switch-outline': `appearance-none
+		[--switch-border-width:var(--ui-switch-border-width,1px)] [border-style:solid] [border-width:var(--switch-border-width)]
+		cursor-pointer w-[--toggle-width] h-[--toggle-height] rounded-[var(--toggle-radius,9999px)]
+		relative before:absolute before:w-[var(--knob-width,var(--knob-size))] before:h-[var(--knob-height,var(--knob-size))] before:content-empty 
+		before:bg-[--knob-bg] before:rounded-[var(--knob-radius,9999px)] ease-linear transition-all duration-300
+		before:left-[--knob-left] before:top-[--knob-top] checked:before:left-[--knob-checked-left] before:ease-linear 
+		before:transition-all before:duration-300 disabled:opacity-70 disabled:cursor-not-allowed 
+		checked:bg-current checked:before:bg-[var(--knob-bg-checked,var(--knob-bg))]`,
+
 		"switch-xs": `${getSwitch("xs")}`,
 		"switch-sm": `${getSwitch("sm")}`,
 		"switch-md": `${getSwitch("md")}`,
 		"switch-lg": `${getSwitch("lg")}`,
+
+		"switch-outline-xs": `${getSwitchOutline("xs")}`,
+		"switch-outline-sm": `${getSwitchOutline("sm")}`,
+		"switch-outline-md": `${getSwitchOutline("md")}`,
+		"switch-outline-lg": `${getSwitchOutline("lg")}`
 	};
 
 	return [switchs];
